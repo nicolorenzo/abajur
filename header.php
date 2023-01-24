72c40e642f36346ec91cc5ccfba6c9e05b4f937e
nicolorenzo <nicolorenzo@live.com>
2023-01-24 14:28:44 -0300

    Page title

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title><?php wp_title(); ?></title>
+    <title><?php wp_title(' | ', 'echo', 'right'); ?><?php bloginfo('name'); ?></title>
     <?php include 'includes/tags.php' ?>
     <?php wp_head() ?>
   </head>

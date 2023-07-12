874d09c8bd2fc57aaaa3e590dd2e0b9ee6899941
Nico Lorenzo <nicolorenzo@live.com>
2023-07-12 11:45:24 -0300

    Google tag manager

--- header.php
+++ header.php
@@ -1,21 +1,11 @@
 <!DOCTYPE html>
 <html class="has-navbar-fixed-top">
   <head>
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-5MN6B72');</script>
-    <!-- End Google Tag Manager -->
     <title><?php wp_title(' | ', 'echo', 'right'); ?><?php bloginfo('name'); ?></title>
     <meta name='description' content='<?php the_field('meta_description') ?>'>
     <?php include 'includes/tags.php' ?>
     <?php wp_head() ?>
   </head>
   <body>
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5MN6B72"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->  
+    <?php if ( function_exists( 'gtm4wp_the_gtm_tag' ) ) { gtm4wp_the_gtm_tag(); } ?>
     <?php include 'includes/nav.php' ?>

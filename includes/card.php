34527a08666735305ccf3a7cac3b741f18d9c63d
nicolorenzo <nicolorenzo@live.com>
2023-11-01 15:15:10 -0300

    Ajuste cards portólio página inicial

--- includes/card.php
+++ includes/card.php
@@ -2,11 +2,13 @@
   $cases = get_field('cases');
   if(get_the_ID() == get_page_by_title('Início')->ID) {
     $cases = get_field('cases', get_page_by_title('Portfólio')->ID);
+    $firstSixElements = array_slice($cases, 0, 6);
+    $cases = $firstSixElements;
   } elseif (get_the_ID() == get_page_by_title('Home')->ID) {
     $cases = get_field('cases', url_to_postid(' http://localhost/abajur/en/portfolio/'));
+      $firstSixElements = array_slice($cases, 0, 6);
+      $cases = $firstSixElements;
   };
-  $firstSixElements = array_slice($cases, 0, 6);
-  $cases = $firstSixElements;
   if(isset($cases)) {
     foreach($cases as $case) {
 ?> 

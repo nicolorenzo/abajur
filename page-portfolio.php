4f614a6b17835f814b14b67b48e190e040996f94
Nico Lorenzo <nicolorenzo@live.com>
2024-10-18 18:35:25 -0300

    Função showCases

--- page-portfolio.php
+++ page-portfolio.php
@@ -16,7 +16,48 @@
       <div class="container">
         <div class="row"> 
         <?php  
-          $cases = get_field('cases'); 
+        function showCases($categoria, $case) {
+          echo '<a href="'.home_url().$case['case_link'].'" class="card animate">
+                  <picture>
+                    <img src="'.$case['case_thumb'].'" alt="" />
+                  </picture>
+                  <div class="card-text animate">
+                    <h3 class="title is-5 has-text-white">'.$case['case_titulo'].'</h3>
+                    <p class="subtitle is-6 mb-2 has-text-white">
+                    '.$case['case_sub_titulo'].'
+                    </p>
+                    <button class="button is-link is-small is-rounded">
+                      <span>Saiba mais</span>
+                    </button>
+                  </div>
+                </a>';
+        }
+          $cases = get_field('cases');
+          foreach($cases as $case) {
+            $categorias = $case['case_categorias'];
+            foreach($categorias as $categoria) {
+              if ($categoria == 'Social Media') {
+                showCases($categoria, $case);
+          /* ?>
+                <a href="<?php echo home_url(); ?><?php echo $case['case_link'] ?>" class="card animate">
+                  <picture>
+                    <img src="<?php echo $case['case_thumb'] ?>" alt="" />
+                  </picture>
+                  <div class="card-text animate">
+                    <h3 class="title is-5 has-text-white"><?php echo $case['case_titulo'] ?></h3>
+                    <p class="subtitle is-6 mb-2 has-text-white">
+                    <?php echo $case['case_sub_titulo'] ?>
+                    </p>
+                    <button class="button is-link is-small is-rounded">
+                      <span>Saiba mais</span>
+                    </button>
+                  </div>
+                </a>
+          <?php  */
+              }
+            }
+            
+          }
           $categorias = $cases[0]['case_categorias'];
             foreach($categorias as $categoria) {
               echo $categoria;
